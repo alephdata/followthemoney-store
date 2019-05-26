4a4814f5546b53a9cdbd9b3d09134c9ab55bb063
Friedrich Lindenberg <friedrich@pudo.org>
2019-05-26 08:20:46 +0200

    Bump version: 0.4.1 → 0.5.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='balkhash',
-    version='0.4.1',
+    version='0.5.0',
     description="Cloud storage library to store raw and structured data from "
                 "different datasets in a data lake",
     long_description="",
