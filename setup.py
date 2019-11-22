b308c5a15477e2bb03de8dcb59d0907274a32b76
Tarashish Mishra <sunu0000@gmail.com>
2019-11-22 17:12:10 +0530

    Bump version: 1.1.1 → 1.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='balkhash',
-    version='1.1.1',
+    version='1.1.2',
     description="Cloud storage library to store raw and structured data from "
                 "different datasets in a data lake",
     long_description="",
