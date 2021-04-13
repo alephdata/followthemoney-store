404f5a7dbe53ecce7c26195be1d8867015be7a5e
Friedrich Lindenberg <friedrich@pudo.org>
2021-04-13 08:07:23 +0200

    Bump version: 3.0.2 → 3.0.3

--- ftmstore/__init__.py
+++ ftmstore/__init__.py
@@ -6,7 +6,7 @@ from ftmstore.dataset import Dataset
 from ftmstore.utils import NULL_ORIGIN
 
 
-__version__ = "3.0.2"
+__version__ = "3.0.3"
 __all__ = ["Dataset", "Store", "get_store", "get_dataset"]
 
 
