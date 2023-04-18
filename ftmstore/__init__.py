1e09162f6c93d676cd393e880144c7319ba792f2
Alex Stefanescu <s.alexandra.stefanescu@gmail.com>
2023-04-18 18:39:33 +0300

    Bump version: 3.0.4 → 3.0.5

--- ftmstore/__init__.py
+++ ftmstore/__init__.py
@@ -6,7 +6,7 @@ from ftmstore.dataset import Dataset
 from ftmstore.utils import NULL_ORIGIN
 
 
-__version__ = "3.0.4"
+__version__ = "3.0.5"
 __all__ = ["Dataset", "Store", "get_store", "get_dataset"]
 
 
